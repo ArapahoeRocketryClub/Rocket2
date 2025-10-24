cbc23c163b1a9751cdf491e7c3cae5bf54ffd3ca
boraayhan <boraayhanwebsite@gmail.com>
2025-10-24 11:36:09 -0600

    Init arduino workspace

--- main.cpp
+++
@@ -1,6 +0,0 @@
-// Name: Main
-// Description: Main code. Includes the state machine.
-
-// Do not sign up for this one right now. We will develop this once the rest are done.
-
-#include 
--- src/termination.h
+++ src/termination.h
@@ -1,5 +1,6 @@
 // Header files are used to ensure that the files can communicate with each other
 #pragma once  // Makes sure this file is included only once during compilation to avoid duplicates
+#include <Arduino.h> // Since .h header files are associated with C++, not pure arduino, we have to include Arduino
 
 // You can define different functions here. They don't have to have ANY code just yet, just the return type and inputs
 //Examples:
