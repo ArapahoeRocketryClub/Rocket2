78914041da65450af8d89002d97ee15ad54d8d82
Ninjanarwhal41 <88503808+Ninjanarwhal41@users.noreply.github.com>
2025-10-30 16:07:03 -0600

    Remove unnecessary comment in data_processing.cpp
    Removed an unnecessary comment from the data processing file.

--- lib/data_processing/data_processing.cpp
+++ lib/data_processing/data_processing.cpp
@@ -2,5 +2,5 @@
 // Description: Functions related to manipulating data received from sensors
 // Devices: N/A, just input from other code
 // Skills: Strong foundations in math and physics. Must be comfortable and enjoy working with vectors, matrices, and numbers. Also requires a strong sense of analyzing inputs/outputs, as most of this deals with converting raw data into usable numbers 
-
+//Comment
 // Dibs: Jordan T.,Drew F,Jacob B
