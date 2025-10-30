8774e1499aa04436b5b835c464f9ce3ab7315ca3
Drew <drefin42@gmail.com>
2025-10-30 08:59:52 -0600

    I added name to file

--- src/data_processing.cpp
+++ src/data_processing.cpp
@@ -3,4 +3,4 @@
 // Devices: N/A, just input from other code
 // Skills: Strong foundations in math and physics. Must be comfortable and enjoy working with vectors, matrices, and numbers. Also requires a strong sense of analyzing inputs/outputs, as most of this deals with converting raw data into usable numbers 
 
-// Dibs: Jordan T.,
+// Dibs: Jordan T.,Drew F
